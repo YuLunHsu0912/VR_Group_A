9471df8cddcc482178d8519630960f3745a6fa8b
YuLunHsu0912 <65931399+YuLunHsu0912@users.noreply.github.com>
2023-11-15 00:40:17 +0800

    Bat basic interaction okay

---
+++ Assets/Scripts/Bat/AudioLoudnessDetection.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLoudnessDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int sampleWindow = 64;
+    private AudioClip microphoneClip;
+ 
+    void Start()
+    {
+        MicrophoneToAudioCLip();
+        Debug.Log(Microphone.devices[0]); ///0 for earphone 1 for oculus
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MicrophoneToAudioCLip()
+    {
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20,AudioSettings.outputSampleRate);
+
+    }
+    public float GetLoudnessFromMicrophone()
+    {
+        return getLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+    public float getLoudnessFromAudioClip(int clipPosition, AudioClip clip)
+    {
+        int startPoisiton = clipPosition - sampleWindow;
+
+        if (startPoisiton < 0)
+            return 0;
+        float[] waveData = new float[sampleWindow];
+        clip.GetData(waveData, startPoisiton);
+
+        //compute loudness
+        float totalLoudness = 0;
+        for (int i=0; i<sampleWindow;i++)
+        {
+            totalLoudness += Mathf.Abs(waveData[i]);
+        }
+        return totalLoudness / sampleWindow;
+    }
+}
---
+++ Assets/Scripts/Bat/Produce_Soundwave.cs
@@ -0,0 +1,43 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Produce_Soundwave : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject myPrefab;
+    public AudioLoudnessDetection detector;
+    public float loudnessSensibility = 100;
+    public float threshold = 10f;
+    public bool KeyboardToProduceSound = true;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+        ///Debug.Log("Now loudness="+loudness);
+        if(KeyboardToProduceSound)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))///
+            {
+                Instantiate(myPrefab, transform.position, Quaternion.identity); //後面加,transform會直接變成這個onject小孩
+            }
+        }
+       
+        else
+        {
+            if(loudness>threshold)///
+            {
+                Instantiate(myPrefab, transform.position, Quaternion.identity); //後面加,transform會直接變成這個onject小孩
+            }
+        }
+
+
+    }
+}
